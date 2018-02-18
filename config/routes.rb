b95a51c9ad40665d79fe5d82f3e97a8b8b834067
user <user@user-no-MacBook-Pro-2.local>
2018-02-18 19:19:32 +0900

    mouitido

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
 
-get 
-get
+  get 'favorites/index'
 
-post
-post
+  get 'sessions/new'
 
+  root 'pages#index'
+  get 'pages/help'
+
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  resources 'users'
+  resources 'topics'
+  post '/favorites', to: 'favorites#create'
+  get '/favorites', to: 'favorites#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
